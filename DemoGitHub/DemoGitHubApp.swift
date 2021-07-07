7f39d73b64ac1e49eefa22aee2bd07251ee76cf8
peterpan <apppeterpan@gmail.com>
2021-07-07 10:10:57 +0800

    Initial Commit

---
+++ DemoGitHub/DemoGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGitHubApp.swift
+//  DemoGitHub
+//
+//  Created by SHIH-YING PAN on 2021/7/7.
+//
+
+import SwiftUI
+
+@main
+struct DemoGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
